c3460c78e249c80723a1127ba790a13238294de1
vigneshNagamuthu <vignesh991995@gmail.com>
2025-08-14 00:46:51 +0800

    add plots

---
+++ Codes/Matlab/plotGPSResults.m
@@ -0,0 +1,97 @@
+close all 
+clear all 
+clc 
+
+%----------------------- Indoor ---------------------------------%
+
+indoorfilename = '../MeasurementResults/gnss_log_20250812_152144 Indoor(Canteen).csv';
+T = readtable(indoorfilename, 'VariableNamingRule', 'preserve');
+
+% Convert timestamp
+T.Timestamp = datetime(T.Timestamp, 'InputFormat', 'yyyy-MM-dd HH:mm:ss');
+
+% Remove rows with missing SNR
+T = T(~isnan(T.SNR), :);
+
+% Get unique PRNs
+prns = unique(T.PRN);
+n = length(prns);
+
+% Layout for subplots
+cols = 3;
+rows = ceil(n / cols);
+
+% Create figure
+figure('Name', 'SNR Over Time - Each PRN');
+
+for i = 1:n
+    prn = prns(i);
+    idx = T.PRN == prn;
+
+    subplot(rows, cols, i);
+    plot(T.Timestamp(idx), T.SNR(idx), 'b-', 'LineWidth', 1.2);
+    title(['PRN ' num2str(prn)]);
+    xlabel('Time');
+    ylabel('SNR (dB)');
+    grid on;
+end
+
+figure('Name', 'Indoor SNR Boxplot');
+allSNR = T.SNR;
+allPRNs = T.PRN;
+
+boxplot(allSNR, allPRNs, 'LabelOrientation', 'inline');
+xlabel('PRN');
+ylabel('SNR (dB)');
+title('Indoor SNR Distribution by Satellite PRN');
+grid on;
+
+
+%------------------------ Outdoor -------------------------------------%
+outdoorfilename = '../MeasurementResults/gnss_log_20250812_153255 Outdoor(Canteen).csv';
+T = readtable(outdoorfilename, 'VariableNamingRule', 'preserve');
+
+% Convert timestamp
+T.Timestamp = datetime(T.Timestamp, 'InputFormat', 'yyyy-MM-dd HH:mm:ss');
+
+% Remove rows with missing SNR
+T = T(~isnan(T.SNR), :);
+
+% Get unique PRNs
+prns = unique(T.PRN);
+n = length(prns);
+
+% Layout for subplots
+cols = 3;
+rows = ceil(n / cols);
+
+% Create figure
+figure('Name', 'SNR Over Time - Each PRN');
+
+for i = 1:n
+    prn = prns(i);
+    idx = T.PRN == prn;
+
+    subplot(rows, cols, i);
+    plot(T.Timestamp(idx), T.SNR(idx), 'b-', 'LineWidth', 1.2);
+    title(['PRN ' num2str(prn)]);
+    xlabel('Time');
+    ylabel('SNR (dB)');
+    grid on;
+end
+
+figure('Name', 'Outdoor SNR Boxplot');
+allSNR = T.SNR;
+allPRNs = T.PRN;
+
+boxplot(allSNR, allPRNs, 'LabelOrientation', 'inline');
+xlabel('PRN');
+ylabel('SNR (dB)');
+title('Outdoor SNR Distribution by Satellite PRN');
+grid on;
+
+
+% ------------------------------------------------------------------- % 
+
+
+
